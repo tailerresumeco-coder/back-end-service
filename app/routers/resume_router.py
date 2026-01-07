3e6212085a97145b33e97393890a2df0a4c47f25
Ranadeep <tailer.resume.co@gmail.com>
2026-01-07 23:31:37 +0530

    keep live api

--- app/routers/resume_router.py
+++ app/routers/resume_router.py
@@ -16,3 +16,20 @@ async def upload_resume(payload: Dict[str, Any]):
     print('Begin resume_router.py -> upload_resume()')
     response = await upload_resume_service(payload)
     return response
+
+@router.post("/tailer-resume")
+async def upload_resume(payload: TailerResumeRequestModel):
+    print('Begin resume_router.py -> upload_resume()')
+    resume = payload.resume
+    jd = payload.jd
+    response = await tailer_resume_service(payload.resume, payload.jd)
+    print('End resume_router.py -> upload_resume()')
+    return response
+
+@router.get("/keepalive")
+async def keep_a_live():
+    try:
+        return "Server is Running..."
+    except:
+        raise HTTPException(status_code=500, detail=str(e))
+  
