442491c09c0c22e20899976277da913386930ea1
Ranadeep <tailer.resume.co@gmail.com>
2026-01-26 21:55:23 +0530

    db commebnted

--- app/services/resume_service.py
+++ app/services/resume_service.py
@@ -233,9 +233,9 @@ async def tailor_resume_groq(
         # Success
         input_tokens = getattr(completion.usage, 'input_tokens', getattr(completion.usage, 'prompt_tokens', 0))
         output_tokens = getattr(completion.usage, 'output_tokens', getattr(completion.usage, 'completion_tokens', 0))
-        groq_collection = groq_tokens_collection.insert_one(
-            {"token": 'mnbvcxzl_token', "count": 1, "tokens": 20,}
-        )
+        # groq_collection = groq_tokens_collection.insert_one(
+        #     {"token": 'mnbvcxzl_token', "count": 1, "tokens": 20,}
+        # )
         return {
             "status": "success",
             "data": parsed_response,
