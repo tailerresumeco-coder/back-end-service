0b9f40023dc7410d25568006afee210d50ccf931
Ranadeep <tailer.resume.co@gmail.com>
2026-01-26 20:24:00 +0530

    cors fix

--- app/main.py
+++ app/main.py
@@ -25,7 +25,8 @@ app.add_middleware(
     allow_origins=[
         "https://tailer-resume.netlify.app",
         "http://localhost:5173",
-        "https://tailerresume.com"
+        "https://tailerresume.com",
+        "www.tailerresume.com"
     ],
     allow_credentials=False,   # ✅ REQUIRED (no cookies/auth used)
     allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
