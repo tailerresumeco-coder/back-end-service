926a9f6423ab9215d86f2086a767f593bf065462
Ranadeep <tailer.resume.co@gmail.com>
2026-01-27 22:21:07 +0530

    db fix

--- app/db.py
+++ app/db.py
@@ -1,17 +1,14 @@
-# app/db.py
-
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 import certifi
 
-MONGO_URI = os.environ["MONGO_URI"] 
+MONGO_URI = os.environ["MONGO_URI"]
 DB_NAME = "tailer_resume"
 
-client = MongoClient(
+client = AsyncIOMotorClient(
     MONGO_URI,
-    tls=True,
     tlsCAFile=certifi.where(),
-    serverSelectionTimeoutMS=30000
+    serverSelectionTimeoutMS=30000,
 )
 
 db = client[DB_NAME]
